63dcc235f63f81b28f1254276e31b35bdd3b5978
vadim <vadim@vadim-VirtualBox.(none)>
2012-05-10 13:22:24 +0300

    W

---
+++ application/modules/default/controllers/SidingController.php
@@ -0,0 +1,21 @@
+<?php
+
+class SidingController extends Zend_Controller_Action
+{
+
+    public function preDispatch(){
+
+    }
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+
+}
+
